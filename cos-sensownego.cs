d028d43c0688b9b65a56e742d13fcce32c0b0547
MinecraftCiach <42491334+MinecraftCiach@users.noreply.github.com>
2020-11-14 13:51:09 +0100

    Create cos-sensownego.cs

---
+++ cos-sensownego.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CosSensownego
+{
+    public class SensownaRzecz
+    {
+        static void Rzecz()
+        {
+            Console.WriteLine("Coś sensownego");
+        }
+    }
+}
